570618d0a94953c66734ebd6ff420ebcc88a938c
nova_core_creatives <robertstrey@hotmail.com>
2022-03-05 00:19:06 -0600

    initial

---
+++ build/precache-manifest.b228bebbd10a1ae5730a2b63f6a1a1bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12902cc30d3238df17caaef49dee4d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a65158a95a90932ea896",
+    "url": "/static/js/2.2294c359.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/static/js/2.2294c359.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f768ebdba6440dec16a",
+    "url": "/static/js/main.b0766a11.chunk.js"
+  },
+  {
+    "revision": "95dcd63a9e5d25e388fa",
+    "url": "/static/js/runtime-main.209acd72.js"
+  }
+]);
